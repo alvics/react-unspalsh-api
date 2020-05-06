c5bd4116a0bc69cbee02f5561eaf3136ca867f57
alvics <alvics71@hotmail.com>
2020-05-06 17:05:38 +1000

    add: unspalsh api keys var

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
+
+const ACCESS_KEY = process.env.REACT_APP_ACCESS_KEY_TOKEN;
 
 class App extends Component {
   onSearchSubmit(term) {
-    console.log(term);
+    axios.get(`https://api.unsplash.com/search/photos`, {
+      params: { query: term },
+      headers: {
+        Authorization: `Client-ID ${ACCESS_KEY}`
+      }
+    });
+    console.log('here is key:', ACCESS_KEY);
   }
 
   render() {
